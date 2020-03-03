c2343dabcf2c9e4cd30d570350c444f7e193a89e
guel99 <49151121+guel99@users.noreply.github.com>
2020-03-03 17:23:17 +0000

    Atualização

--- Backend/ConsultaJa/ConsultaJa/Program.cs
+++ Backend/ConsultaJa/ConsultaJa/Program.cs
@@ -11,7 +11,9 @@ namespace ConsultaJa
         static void Main(string[] args)
         {
             ConsultaJaModel cjm = new ConsultaJaModel();
-            cjm.novoMedico("Miguel Oliveira", "migueloliveira985@gmail.com", "sdhawfebfse", null, new DateTime(1999, 08, 07), "Rua do Cruzeiro", "266951155");
+            string res = cjm.novoMedico("Miguel Oliveira", "migueloliveira985@gmail.com", "sdhawfebfse", null, new DateTime(1999, 08, 07), "Rua do Cruzeiro", "266951155");
+            Console.WriteLine("Id atribuido: " + res);
+            cjm.login("M0", "migueloliveira985@gmail.com", "hello");
         }
     }
 }
