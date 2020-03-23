5a1db1582d12f04ffa315fefe62e078fd7b1f29a
guel99 <49151121+guel99@users.noreply.github.com>
2020-03-23 15:03:59 +0000

    Configuração da base de dados

---
+++ configsDB.sql
@@ -0,0 +1,6 @@
+Insert into codigo_postal (codigo_postal,localidade) values ('4730-280','Braga');
+
+insert into configs (parametro, value) values ('medicos',0);
+insert into configs (parametro, value) values ('pacientes',0);
+insert into configs (parametro, value) values ('preco',15000);
+insert into configs (parametro, value) values ('admin.code',147852369);
