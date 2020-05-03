76c16cc5e9aa5c39d3c28ececa7ffe7e831ec985
Nelson Faria <49026704+nelson31@users.noreply.github.com>
2020-05-03 01:34:22 +0100

    alteracoes

--- FrontEnd/ConsultaJa/ConsultaJa/Models/Exceptions/ClassificacaoInvalida.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class ClassificacaoInvalida : Exception
-{
-	public ClassificacaoInvalida() : base() { }
-
-	public ClassificacaoInvalida(string message) : base(message) { }
-}
--- FrontEnd/ConsultaJa/ConsultaJa/Models/Exceptions/MailNaoRegistado.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class MailNaoRegistado : Exception
-{
-	public MailNaoRegistado() : base() { }
-
-	public MailNaoRegistado(string message) : base(message) { }
-}
